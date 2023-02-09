465c77178ab50eb73d3e7d39d778b7265a8fea10
Sukanvisa <sukanvisa.pearyoo@gmail.com>
2023-02-09 16:48:03 -0500

    commiting all changes first half of class 2/9

---
+++ model/data-layer.php
@@ -0,0 +1,7 @@
+<?php
+function getMeals(){
+    return array("breakfast", "lunch", "dinner","dessert");
+}
+function getCondiment(){
+    return array("Ketchup", "Mustard", "Sriracha","Kimchi","Mayo","Sweet and Spicy");
+}
